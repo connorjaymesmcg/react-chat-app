c73662e2178c4cadc103255b50d90d47e46dbe79
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-10-02 12:58:39 -0400

    Sidebar created

--- client/src/Components/ChannelListContainer.jsx
+++ client/src/Components/ChannelListContainer.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import SwordIcon from '../assets/swords.png';
+import LogoutIcon from '../assets/logout.png';
+
+
+const SideBar = () => {
+  return (
+    <div className='channel-list__sidebar'>
+      <div className='channel-list__sidebar__icon1'>
+        <div className='icon1__inner'>
+          <img src={SwordIcon} alt="swords" width='30' />
+        </div>
+      </div>
+      <div className='channel-list__sidebar__icon2'>
+        <div className='icon1__inner'>
+          <img src={LogoutIcon} alt="logout" width='30' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CompanyHeader = () => {
+  return (
+    <div className='channel-list__header'>
+      <p className='channel-list__header__text'> BattleChat </p>
+    </div>
+  );
+};
 
 const ChannelListContainer = () => {
   return (
-    <div>
-      Channel List
-    </div>
-  )
-}
+    <>
+      <SideBar />
+      <div className='channel-list__list__wrapper'>
+        <CompanyHeader />
+      </div>
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
---
+++ client/src/Components/ChannelSearch.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { SearchIcon } from '../assets';
+
+const ChannelSearch = () => {
+  return (
+    <div className='channel-search__container'>
+
+    </div>
+  );
+};
+
+export default ChannelSearch;

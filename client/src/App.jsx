8908ab0d708b062e9234d0c410c19b97bc4ff43f
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-10-02 15:03:32 -0400

    Created login/signup splash page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelContainer, ChannelListContainer } from './Components';
+import { ChannelContainer, ChannelListContainer, Auth } from './Components';
 import './App.css';
 
 const apiKey = process.env.REACT_APP_STREAM_KEY;
@@ -11,7 +11,12 @@ const apiKey = process.env.REACT_APP_STREAM_KEY;
 // Create instance of Stream chat with API key
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+
+  if (!authToken) return <Auth />;
+
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>

573f80196bfbd78b9b549b2f29d440198aa9e541
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-10-02 12:34:18 -0400

    Added basic file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import ChannelContainer from './Components/ChannelContainer';
+import ChannelListContainer from './Components/ChannelListContainer';
+
+const apiKey = process.env.REACT_APP_STREAM_KEY;
+
+// Create instance of Stream chat with API key
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>React Chat</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team light'>
+        <ChannelContainer />
+        <ChannelListContainer />
+      </Chat>
     </div>
   );
 };
